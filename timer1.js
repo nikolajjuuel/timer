2e68d44b0a933774d51c5dd348476cffdfac202d
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-09-21 21:36:21 +0000

    timer init

---
+++ timer1.js
@@ -0,0 +1,20 @@
+// const timer = []
+const arr = process.argv.slice(2);
+
+time = arr.map((x) => x * 1000);
+
+const timer = function (time) {
+  for (let i = 0; i < time.length; i++) {
+    setTimeout(() => {
+      const noise = process.stdout.write('\x07')
+      console.log(`noise ${time[i]/1000} second timer ${noise}`);
+    }, time[i])
+  }
+
+};
+timer(time);
+
+
+
+// const noise = process.stdout.write('\x07');
+// console.log(noise);
